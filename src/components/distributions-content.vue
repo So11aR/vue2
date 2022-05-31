bd37c86d26b1e0bedef19dacd2171b7e63e7523d
a-girl-who-codes <beatrixlinkoln@gmail.com>
2022-05-31 23:36:45 +0300

    Filter empty items

--- src/components/distributions-content.vue
+++ src/components/distributions-content.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
     <div
-        v-if="hasLicenses"
+        v-if="item.modules_licenses.length > 0"
         class="download_distribution distribution-text"
         @click="downloadLicences"
     >Загрузить лицензии</div>
-    <div v-if="!product.distribution" class="no-distribution">
-      Нет доступных дистрибутивов
-    </div>
-    <div v-for="(el, index) in product.distribution" :key="index" class="download_distribution" @click="download(el.id)">
+    <div v-for="(el, index) in item.distribution" :key="index" class="download_distribution" @click="download(el.id)">
       <span class="distribution-text">Загрузить дистрибутив {{ el.architecture }}</span>
       <span class="distribution-size">{{ el.size }}</span>
     </div>
@@ -23,43 +20,16 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-      product: {
-        id: this.item.id,
-        modules_licenses: [],
-        links: {
-          product_versions: this.item.links.product_versions
-        }
-      },
-      hasLicenses: true,
-    }
-  },
   methods: {
     download(distributionId) {
       this.$emit('change-distribution', distributionId);
-
-      if (this.hasLicenses) {
-        this.$emit('checkbox-all', this.product.modules_licenses.map(el => el.id));
-      }
+      this.$emit('checkbox-all', this.item.modules_licenses.map(el => el.id));
       this.$emit('create-license-archive');
     },
     downloadLicences() {
-      if (!this.hasLicenses) return;
-      this.$emit('checkbox-all', this.product.modules_licenses.map(el => el.id));
+      this.$emit('checkbox-all', this.item.modules_licenses.map(el => el.id));
       this.download('not download');
     }
   },
-  mounted() {
-    fetch(`https://update.wizardsoft.ru${this.product.links.product_versions}`)
-      .then((response) => response.json())
-      .then((data) => {
-         this.hasLicenses = Boolean(data.length && data[0].modules_licenses.length);
-         if (data[0]) {
-             this.product = data[0];
-         }
-      })
-      .catch((err) => console.log(err))
-  }
 };
 </script>
--- src/store/modules/app-table-module.js
+++ src/store/modules/app-table-module.js
@@ -9,9 +9,17 @@ export default {
   },
   actions: {
     async fetchLicenses({ commit, rootState }) {
-      return fetch(` https://update.wizardsoft.ru/api/serial/${rootState.code}/products`)
+      return fetch(`https://update.wizardsoft.ru/api/serial/${rootState.code}/products`)
         .then(response => response.status === 200 ? response.json() : Promise.reject())
-        .then(licenses => commit("setLicense", licenses))
+        .then((licenses) => {
+          const promises = licenses
+              .map(url => fetch(`https://update.wizardsoft.ru${url.links.product_versions}`).then(r => r.json()));
+
+          Promise.all(promises).then((data) => {
+            const notEmpty = data.filter(el => Boolean(el.length)).flat();
+            return commit("setLicense", notEmpty)
+          });
+        })
         .catch(reason => commit("setErrorGlobal", reason || true, {root: true}))
     },
   },
